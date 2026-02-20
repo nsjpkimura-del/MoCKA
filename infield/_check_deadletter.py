01ab4ba865ab420c8b4bd24278296eb0ff2b103d
NSJP_kimura <m_kimura@nsjp.org>
2026-02-20 18:28:02 +0900

    Phase1: minimize public by removing execution core

--- infield/_check_deadletter.py
+++
@@ -1,8 +0,0 @@
-﻿import os, sqlite3
-db=os.environ["MOCKA_DB"]
-con=sqlite3.connect(db)
-cur=con.cursor()
-rid="R-LOCKTEST-2026-02-17_010312"
-cur.execute("SELECT replan_request_id, status, retry_count, max_retry, next_retry_at, error_class FROM retry_queue WHERE replan_request_id=?", (rid,))
-print("AFTER_RUN:", cur.fetchone())
-con.close()
