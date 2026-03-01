fc3f4852c500136ba740bf68363498507c048e60
NSJP_kimura <m_kimura@nsjp.org>
2026-03-01 16:00:58 +0900

    Sample05: add one-shot verifier script

---
+++ transparency/sample05/verify_sample05.ps1
@@ -0,0 +1,10 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+cd "C:\Users\sirok\MoCKA\transparency\sample05"
+
+if(-not (Test-Path ".\time_target.txt")) { throw "missing time_target.txt" }
+if(-not (Test-Path ".\response.tsr")) { throw "missing response.tsr" }
+if(-not (Test-Path ".\tsa_cert.pem")) { throw "missing tsa_cert.pem" }
+
+openssl ts -verify -data time_target.txt -in response.tsr -CAfile tsa_cert.pem -untrusted tsa_cert.pem
